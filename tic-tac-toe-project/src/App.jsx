f0a77960b1188f7cfb4af022c8d87894a17a1efd
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-01-25 18:57:49 +0100

    Working model of X, O game

--- tic-tac-toe-project/src/App.jsx
+++ tic-tac-toe-project/src/App.jsx
@@ -1,16 +1,46 @@
 import { useState } from "react";
 
+function Square({ value, onSquareClick }) {
+  function handleClick() {
+    setValue("X"); //this set function is telling React to re-render that square whenever it's <button> is clicked
+  }
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  ); //rendering the value variable from your component, a placeholder to show the current state in the button element
+}
+
 export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null)); //want an array with 9 in it and fill each of these 9 indexes with null
+  const [xIsNext, setxIsNext] = useState(true)
   //function to add an x at whatever index is clicked
+  const [squares, setSquares] = useState(Array(9).fill(null)); //want an array with 9 in it and fill each of these 9 indexes with null
+ 
   function handleClick(i) {
-    const nextSquares = squares.slice(); //creates a new array
-    nextSquares[i] = "X";
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice(); //creates a copy of squares array
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares); //lets react know the state of the component has changed
+    setxIsNext(!xIsNext) //state updater function, the state is oppositive of its current value e.g. if true/X, then next time it will be false/O
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -30,15 +60,24 @@ export default function Board() {
   );
 }
 
-function Square({ value, onSquareClick }) {
-  function handleClick() {
-    setValue("X"); //this set function is telling React to re-render that square whenever it's <button> is clicked
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  ); //rendering the value variable from your component, a placeholder to show the current state in the button element
+  return null;
 }
 
 
